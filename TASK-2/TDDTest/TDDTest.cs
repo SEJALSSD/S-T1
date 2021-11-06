fe41e8d287a5c966b6a4565a24c87b29bc201ec6
AVITA <sejaldeshpande17@gmail.com>
2021-11-07 02:55:33 +0530

    Add project files.

---
+++ TASK-2/TDDTest/TDDTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using myApps;
+
+namespace MyTest
+{
+    [TestFixture]
+    public class TDDTestCalculation 
+    {
+        [Test]
+        public void Calculator_MultiplicationT()
+        {
+            Calculation calculation = new Calculation();
+
+            Assert.AreEqual(12, calculation.SI(3, 2, 2));
+            Assert.AreEqual(1220, calculation.SI(5, 122, 2));
+
+
+        }
+
+        [Test]
+        public void Calculator_Division()
+        {
+            Calculation calculation = new Calculation();
+            Assert.AreEqual(3, calculation.Di( 6, 2));
+            Assert.AreEqual(-9999, calculation.Di(20, 0));
+            Assert.AreEqual(0, calculation.Di(8, 0));
+        }
+
+       
+
+
+    }
+}
---
+++ TASK-2/myApps/Calculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myApps 
+{
+    public class Calculation
+
+    {
+        public static int answer ;
+
+        public int SI(int p,int q,int r)
+        {
+            
+            return p*q*r;
+        }
+
+        public int Di(int a , int b)
+        {
+            answer = -9999;
+
+            if (b == 0)
+            {
+                Console.WriteLine("Warning : The second number could not be 0");
+              
+                return answer;
+            }
+            else
+            {
+                answer = a / b;
+                return answer;
+            }
+        }
+         
+    }
+}
